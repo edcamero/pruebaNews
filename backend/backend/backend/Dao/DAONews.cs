398ddb8b0a9376176082d6acd015acd933192e48
Enyerson Camero <blade_liger@hotmail.com>
2021-01-24 06:44:34 -0500

    first commit

---
+++ backend/backend/backend/Dao/DAONews.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NewsAPI;
+using NewsAPI.Constants;
+using NewsAPI.Models;
+
+
+namespace backend.Dao
+{
+    public class DAONews
+    {
+        public DAONews getCityNews (string city)
+        {
+            DAONews daoNews = new DAONews();
+
+            return daoNews;
+        }
+    }
+}
